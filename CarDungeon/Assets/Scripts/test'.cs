41cfda1702fa05880d9afd42501a8fb22c893ebd
PacifistWaffle <PacifistWaffle@gmail.com>
2023-09-09 17:06:55 +0200

    test dla sejmeja

--- CarDungeon/Assets/Scripts/test'.cs
+++ CarDungeon/Assets/Scripts/test'.cs
@@ -2,13 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BasicBehavior : MonoBehaviour
+public class test' : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-
-    }   
+        
+    }
 
     // Update is called once per frame
     void Update()
