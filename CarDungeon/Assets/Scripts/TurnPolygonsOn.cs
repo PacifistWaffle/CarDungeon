37ae051270b10aacc051c49884710960454315e7
PacifistWaffle <filipp12292004@gmail.com>
2023-10-10 08:53:01 +0200

    sperma

---
+++ CarDungeon/Assets/Scripts/TurnPolygonsOn.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TurnPolygonsOn : MonoBehaviour
+{ 
+    public void OnClick()
+    {
+
+    }
+}
