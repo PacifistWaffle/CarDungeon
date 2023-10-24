3ed69d52b44ccb2995c87513a0d507401ef9158b
PacifistWaffle <filipp12292004@gmail.com>
2023-10-24 08:03:36 +0200

    yep

--- CarDungeon/Assets/Scripts/TurnPolygonsOn.cs
+++ CarDungeon/Assets/Scripts/TurnPolygonsOn.cs
@@ -7,9 +7,17 @@ using UnityEngine.UI;
 
 public class TurnPolygonsOn : MonoBehaviour
 {
+    GameObject card;
+    PolygonCollider2D poly;
+    BoxCollider2D box;
 
     public void OnButtonPress()
-    { 
+    {
+        card = GameObject.FindGameObjectWithTag("placedCard");
+        poly = card.GetComponent<PolygonCollider2D>();
+        box = card.GetComponent<BoxCollider2D>();
 
+        box.enabled = false;
+        poly.enabled = true;
     }
 }
